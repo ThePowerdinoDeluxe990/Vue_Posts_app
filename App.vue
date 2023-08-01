2286abaa95e32ae2dbe5a1b0a16dde44511f7dd2
ThePowerdinoDeluxe990 <118630450+ThePowerdinoDeluxe990@users.noreply.github.com>
2023-08-01 19:01:57 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import ListComponent from './components/products/ListComponent.vue'
+</script>
+
+<template>
+  <div class="tree">
+     <div class="Head">
+       <h1>Posts</h1>
+    </div>
+
+    <ListComponent/>
+  </div>  
+</template>
+
+<style lang="scss">
+  @import './App.scss';
+</style>
+
+
---
+++ components/products/ListComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="Post">
+            
+        <div class="Body" v-for="p in post" :key="p.id" >
+            <h2>{{ p.title }}</h2>
+            <p>{{ p.body }}</p>
+        </div>
+            
+    </div>
+</template>
+
+<style lang="scss">
+    @import './ListComponent.scss';
+</style>
+
+<script lang="ts">
+  export default{
+    
+    data(){
+        return{
+            post:<any>[],
+            api:<string> "https://jsonplaceholder.typicode.com/posts?userId=1"
+        }
+    },
+    methods:{
+        getPosts(){
+            fetch(this.api)
+            .then(reponse => reponse.json())
+            .then(data => this.post = data)
+        },
+    },
+    mounted(){
+        this.getPosts()
+    },
+  }
+</script>
